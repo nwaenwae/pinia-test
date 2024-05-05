c09f90aba9438a7ea336c9a995a72e2540284768
NwaeNandarAung97 <nandar.a@codetism.com>
2024-05-05 19:42:08 +0700

    state and getter

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
-
 <template>
-  <header>
-    <img src="./assets/pinia-logo.svg" alt="pinia logo">
-    <h1>Pinia Tasks</h1>
-  </header>
-
   <main>
-
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia logo">
+      <h1>Pinia Tasks</h1>
+    </header>
+    <nav class="filter">
+        <button @click="filter = 'all'">All tasks</button>
+        <button @click="filter = 'favs'">Fav tasks</button>
+    </nav>
+    <div class="task-list" v-if="filter === 'all'">
+      <p>You have {{taskStore.totalCount}} tasks left to do</p>
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetails :task="task"/>
+      </div>
+    </div>
+    <div class="task-list" v-if="filter === 'favs'">
+      <p>You have {{taskStore.favCount}} tasks left to do</p>
+      <div v-for="task in taskStore.favs" :key="task.id">
+        <TaskDetails :task="task"/>
+      </div>
+    </div>
   </main>
 </template>
+
+<script setup>
+import {ref} from "vue";
+import {useTaskStore} from "@/stores/TaskStore.js";
+import TaskDetails from "@/components/TaskDetails.vue";
+
+const filter = ref('all')
+const taskStore = useTaskStore()
+</script>
 
